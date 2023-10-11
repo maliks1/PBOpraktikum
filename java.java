22ac501449edcfecc07e95cca48432a4991bb0e3
maliks1 <115067646+maliks1@users.noreply.github.com>
2023-10-11 18:56:09 +0700

    Add files via upload

---
+++ java.java
@@ -0,0 +1,10 @@
+public class SegitigaBintang {
+  public static void main(String args[]){
+
+    System.out.println("##  Program Java Segitiga Bintang  ##");
+    System.out.println("=====================================");
+    System.out.println();
+
+    }
+     
+  }
